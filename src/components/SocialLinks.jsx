49d60fe51a24355ed818d39afe40c28448e934c3
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-04-01 23:19:42 +0530

    first commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,70 +1,79 @@
-import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
+import React from "react";
+import { FaYoutube, FaInstagram } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          YouTube <FaYoutube size={25} />
+        </>
+      ),
+      href: "https://www.youtube.com/@MegaheartzIIITBhubaneswar",
+      target: "_blank",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Instagram <FaInstagram size={25} />
+        </>
+      ),
+      href: "https://www.instagram.com/megaheartz_iiitbh?igsh=YmRnZGt5engzY2x0",
+      target: "_blank",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={25} />
+        </>
+      ),
+      href: "megaheartz@iiit-bh.ac.in",
+      target: "_blank",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Report <BsFillPersonLinesFill size={25} />
+        </>
+      ),
+      href: "https://drive.google.com/file/d/1JbKwgAOvvMhk5gusnDbwPiOzRV9D8kOL/view?usp=sharing",
+      style: "rounded-br-md",
+      download: true,
+      target: "_blank",
+    },
+  ];
 
-    const links = [
-        {
-            id: 1,
-            child: (
-                <>
-                    GitHub <FaGithub size={25} />
-                </>
-            ),
-            href: "https://www.github.com/dipayansarkar47",
-            style: "rounded-tr-md"
-        },
-        {
-            id: 2,
-            child: (
-                <>
-                    LinkedIn <FaLinkedin size={25} />
-                </>
-            ),
-            href: "https://www.linkedin.com/in/dipayan-sarkar-ab465b1b5/",
-        },
-        {
-            id: 3,
-            child: (
-                <>
-                    Mail <HiOutlineMail size={25} />
-                </>
-            ),
-            href: "mailto:dipayansarkar265482@gmail.com",
-        },
-        {
-            id: 4,
-            child: (
-                <>
-                    Resume <BsFillPersonLinesFill size={25} />
-                </>
-            ),
-            href: "/Dipayan's Resume.pdf",
-            style: "rounded-br-md",
-            download: true
-        },
-    ]
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:rounded-md hover:ml-[-10px] duration-300" +
+              style
+            }
+          >
+            <a
+              href={href}
+              download={download}
+              className="flex justify-between items-center w-full text-white"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
-            <ul>
-                {links.map(({id, child, href, style, download}) => (
-                    <li key={id}
-                        className={"flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:rounded-md hover:ml-[-10px] duration-300" + style}>
-                        <a href={href}
-                           download={download}
-                           className="flex justify-between items-center w-full text-white"
-                           target="_blank"
-                           rel="noreferrer">
-                            {child}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-
-export default SocialLinks
+export default SocialLinks;
