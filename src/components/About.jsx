6a191469b66de8467bb823ca51f4b42f0f493805
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-04-02 10:28:39 +0530

    email-js implemented

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,43 +1,75 @@
-import React, { useEffect } from 'react'
-import { AiOutlineCloudDownload } from 'react-icons/ai'
-import '../App.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import { AiOutlineCloudDownload } from "react-icons/ai";
+import "../App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
-    useEffect(() => {
-        AOS.init({ duration: 1000 })
-    })
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  });
 
-    return (
-        <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white'>
-            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
-                <div className='pb-8'>
-
-                    <h2 className='text-4xl  font-bold inline border-b-4 border-gray-500'>About</h2>
-
-                </div>
-
-                <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-                    I embarked my journey as a B.Tech Computer Science & Engineering student at Ramkrishna Mahato Government Engineering College in 2020. Along the way, I fearlessly explored various fields, including Graphic Designing, Content Creation, Freelancing, and Entrepreneurship. I delved into the world of cutting-edge technologies, such as Python, Cloud computing, React, Flutter, and Firebase. I also took my passion for technology and leadership to new heights by co-establishing Google Developer Student Clubs and the E-Cell in my college, alongside my equally enthusiastic classmates. At present, I serve GDSC RKMGEC as the Co-Lead and holds the significant responsibility of being the In-Charge of E-Cell RKMGEC.
-                </p>
-                <br />
-                <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-                    In a constant pursuit of sharing knowledge and fostering growth, I've organized numerous technical workshops, imparting valuable insights on technologies like Python, Android, and Google Cloud to aspiring learners. With a commitment to personal development and a drive for making a positive impact within the tech community.
-                </p>
-                {/* <br />
-                <p data-aos="fade-in" data-aos-duration="500" className='text-xl mt-5 ease-in  fade-in'>
-                    I've worked for many projects for my clients as well as my personal projects. I also contributed for the GDSC RKMGEC web page. I have a Instagram page (@codewithbiki) where I create content for those who are just starting with programming and have more that 70k followers. I have learning attitude and growth mindset which helps me to collaborate with people and work for the society.
-                </p> */}
-                <div data-aos="zoom-in" data-aos-duration="500" className='mb-20 fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'>
-
-
-                    <AiOutlineCloudDownload />
-                    <a href="/" download>Download CV</a>
-                </div>
-            </div>
+  return (
+    <div
+      name="about"
+      className="w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto">
+        <div className="pb-8 text-center">
+          <h2 className="text-4xl  font-bold inline border-b-4 border-gray-500">
+            What is MegaHeartz ?
+          </h2>
         </div>
-    )
-}
 
-export default About
+        <p
+          data-aos="slide-up"
+          data-aos-duration="500"
+          className="text-xl mt-5 ease-in fade-in"
+        >
+          Working as the face of IIIT Bhubaneswar to showcase the college to the
+          outer world via the virtual network thus increasing its brand value.
+        </p>
+        <p
+          data-aos="slide-up"
+          data-aos-duration="500"
+          className="text-xl mt-5 ease-in fade-in"
+        >
+          Was started by Suraj Sharma Sir, in 2022 because our college didn’t
+          have an official platform via which the outer world can know what’s
+          happening or have happened in IIIT Bhubaneswar.
+        </p>
+        <p
+          data-aos="slide-up"
+          data-aos-duration="500"
+          className="text-xl mt-5 ease-in fade-in"
+        >
+          Established as a society in the year: 2023.
+        </p>
+        <p
+          data-aos="slide-up"
+          data-aos-duration="500"
+          className="text-xl mt-5 ease-in fade-in"
+        >
+          MegaHeartz has tried to cover nearly all the formal and informal
+          events happening in the college and is also contributing actively and
+          in a full-fledged manner for student media team of IIIT Bhubaneswar by
+          covering all the events and providing the content (videos, photos,
+          writeups, etc.) for each and every single post, to enhance the
+          official social media handles of IIIT-Bh (Instagram, Twitter,
+          Facebook, LinkedIn).
+        </p>
+        <div
+          data-aos="zoom-in"
+          data-aos-duration="500"
+          className="mb-20 fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-pink-500 to-purple-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2"
+        >
+          <a href="https://drive.google.com/file/d/1JbKwgAOvvMhk5gusnDbwPiOzRV9D8kOL/view?usp=sharing">
+            Society Report (2023-24)
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,92 @@
-import React, {useEffect} from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    useEffect(()=> {
-        AOS.init({duration: 1000})
-    })
-    return (
-        <div name='contact' className='w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
-            <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-screen'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-gray-400'>Contact Me</p>
-                    <p className='mt-6'>Submit the form below to get in touch with me</p>
-                </div>
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  });
 
-                <div className='flex justify-center items-center'>
-                    <form action="" method="POST" className='flex flex-col w-full md:w-1/2'>
-                        <input data-aos="slide-up" data-aos-duration="500" required type="text" name='name' placeholder='Enter your name' className='p-2 m-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                        <input data-aos="slide-up" data-aos-duration="700" required type="email" name='email' placeholder='Enter your e-mail' className='p-2 m-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                        <textarea data-aos="fade-in" data-aos-duration="900" name="message" required rows="10" placeholder='Enter your message' className='p-2 bg-transparent border-2 m-2 rounded-md text-white focus:outline-none'></textarea>
-                        <button data-aos="zoom-in" data-aos-duration="1000" type="submit" className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's Talk</button>
-                    </form>
-                </div>
-            </div>
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log("Email sent successfully:", result.text);
+          // Add your success message or redirect here
+        },
+        (error) => {
+          console.error("Email sending failed:", error.text);
+          // Add your error message or handling here
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <div
+      name="contact"
+      className="w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-screen">
+        <div className="pb-8 text-center">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-400">
+            Reach Out
+          </p>
+          <p className="mt-6">Get in touch with us</p>
         </div>
-    )
-}
 
-export default Contact
+        <div className="flex justify-center items-center">
+          <form onSubmit={sendEmail} className="flex flex-col w-full md:w-1/2">
+            <input
+              data-aos="slide-up"
+              data-aos-duration="500"
+              required
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 m-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              data-aos="slide-up"
+              data-aos-duration="700"
+              required
+              type="email"
+              name="email"
+              placeholder="Enter your e-mail"
+              className="p-2 m-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              data-aos="fade-in"
+              data-aos-duration="900"
+              name="message"
+              required
+              rows="10"
+              placeholder="Enter your message"
+              className="p-2 bg-transparent border-2 m-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+            <button
+              data-aos="zoom-in"
+              data-aos-duration="1000"
+              type="submit"
+              className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+            >
+              Let's Talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
